60dbb6ed2af945816518c6e0d8d94e7a7ee9e27b
anastas4483 <nastasya.sorokina1@gmail.com>
2022-02-13 20:32:17 +0300

    completed borderRadius & BG of button in PersRoom

--- components/PersRoom.js
+++ components/PersRoom.js
@@ -1,16 +1,10 @@
-import { Text, View, Image, Touchable, Pressable } from "react-native"
+import { Text, View, Image,  Pressable, ImageBackground } from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
 import { useState } from "react"
 import { stylePers } from "../styles/persRoom"
 import { gStyle } from "../styles/gStyle"
 
-import Form from "./Form"
-import {
-  TextInput,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-} from "react-native-web"
+
 export default function Login() {
   const onPressHandler = () => {
     console.log("click")
@@ -24,12 +18,29 @@ export default function Login() {
         <View style={stylePers.persHeader}>
           <Text style={stylePers.persHeaderText}>Добрый день, Андрей!</Text>
           <Pressable onPress={onPressHandler}>
-              <Image
-                source={require("../assets/gear.png")}
-                style={stylePers.persHeaderGear}
-              />
+            <Image
+              source={require("../assets/gear.png")}
+              style={stylePers.persHeaderGear}
+            />
           </Pressable>
+        </View>
 
+        <View style={stylePers.persWrap}>
+          <View style={stylePers.persNots}>
+            <View style={stylePers.persNotsList}>
+
+            </View>
+           <Pressable onPress={()=> console.log('mfsm')} style={stylePers.persNotsWrapButton}>
+              {/* <LinearGradient start={{x: 0.0, y: 0.25}} end={{x: 0.5, y: 1.0}} colors={["#5A8FF8",  '#6695F6',"#4A81F0","#386CD2",]} style={stylePers.persNotsButton}> */}
+              <ImageBackground source={ require("../assets/prersBgButton.png" ) } imageStyle={stylePers.persBGImage} style= {[{width: '100%', height: '100%', }, stylePers.persBGImage]} >
+                <Text style={stylePers.persNotsButtonText}>
+                Показать все...
+              </Text>
+              </ImageBackground>
+            {/* </LinearGradient> */}
+            </Pressable>
+            
+          </View>
         </View>
       </LinearGradient>
     </View>
--- styles/persRoom.js
+++ styles/persRoom.js
@@ -1,28 +1,74 @@
 import { StyleSheet, Platform } from "react-native"
 
 export const stylePers = StyleSheet.create({
-    persHeader:{
-        width:'100%',
-        backgroundColor: '#355D8B',
-        paddingTop: 35,
-        paddingBottom:15,
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-        top: 0,
-        alignItems: 'center'
-        
-    },
-    persHeaderText: {
-        fontFamily: 'os_reg',
-        fontSize: 18,
-        color: '#fff',
-        
-    },
-    persHeaderGear: {
-        width: 30,
-        height:30,
-        
-    }
+  persHeader: {
+    width: "100%",
+    backgroundColor: "#355D8B",
+    paddingTop: 35,
+    paddingBottom: 15,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    top: 0,
+    alignItems: "center",
+  },
+  persHeaderText: {
+    fontFamily: "os_reg",
+    fontSize: 18,
+    color: "#fff",
+  },
+  persHeaderGear: {
+    width: 30,
+    height: 30,
+  },
+  persWrap: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 40,
+    justifyContent: "space-between",
+  },
+  persNots: {
+    backgroundColor: "#91B1D7",
+    borderRadius: 20,
+    width: "80%",
+    height: 175,
+    marginHorizontal: "auto",
+    shadowOpacity: 0.75,
+    shadowRadius: 0,
+    shadowColor: "#28466A",
+    shadowOffset: { height: 5, width: 0 },
+    marginHorizontal: "5%",
+    elevation: 5,
+  },
+  persNotsList: {
+    backgroundColor: "#91B1D7",
+    borderRadius: 20,
+    width: "80%",
+    height: 175,
+    marginHorizontal: "auto",
+    flex: 7,
+  },
+  persNotsWrapButton: {
+    // backgroundImage: "url()",
+    width: "100%",
+    height: 50,
+    flex: 2,
+    borderBottomEndRadius: 20,
+    borderBottomStartRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+    zIndex: 100,
+    overflow: 'hidden'
+  },
 
+  persNotsButtonText: {
+    color: '#fff'
+  },
+  persBGImage:{
+      alignItems: 'center',
+      justifyContent: "center",
+    
+  
+  }
 })

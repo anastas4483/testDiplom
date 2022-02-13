2fa33494a7a9610947c9f333d722df1d33be79c7
anastas4483 <nastasya.sorokina1@gmail.com>
2022-02-13 22:26:57 +0300

    add capinet level in persRoom

--- components/PersRoom.js
+++ components/PersRoom.js
@@ -1,9 +1,8 @@
-import { Text, View, Image,  Pressable, ImageBackground } from "react-native"
+import { Text, View, Image, Pressable, ImageBackground } from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
 import { useState } from "react"
 import { stylePers } from "../styles/persRoom"
 import { gStyle } from "../styles/gStyle"
-
 
 export default function Login() {
   const onPressHandler = () => {
@@ -14,6 +13,7 @@ export default function Login() {
       <LinearGradient
         colors={["#3F70A8", "#4B84C5", "#4F8BD0"]}
         style={[gStyle.gradient, { paddingTop: 0 }]}
+        start={{x: 0, y: 1}} end={{x: 0, y: -1}}
       >
         <View style={stylePers.persHeader}>
           <Text style={stylePers.persHeaderText}>Добрый день, Андрей!</Text>
@@ -26,20 +26,52 @@ export default function Login() {
         </View>
 
         <View style={stylePers.persWrap}>
-          <View style={stylePers.persNots}>
-            <View style={stylePers.persNotsList}>
-
-            </View>
-           <Pressable onPress={()=> console.log('mfsm')} style={stylePers.persNotsWrapButton}>
+          <View style={[stylePers.persNots, stylePers.persLevels]}>
+            <View style={stylePers.persNotsList}></View>
+            <Pressable
+              onPress={() => console.log("mfsm")}
+              style={stylePers.persNotsWrapButton}
+            >
               {/* <LinearGradient start={{x: 0.0, y: 0.25}} end={{x: 0.5, y: 1.0}} colors={["#5A8FF8",  '#6695F6',"#4A81F0","#386CD2",]} style={stylePers.persNotsButton}> */}
-              <ImageBackground source={ require("../assets/prersBgButton.png" ) } imageStyle={stylePers.persBGImage} style= {[{width: '100%', height: '100%', }, stylePers.persBGImage]} >
+              <ImageBackground
+                source={require("../assets/prersBgButton.png")}
+                imageStyle={stylePers.persBGImage}
+                style={[
+                  { width: "100%", height: "100%" },
+                  stylePers.persBGImage,
+                ]}
+              >
                 <Text style={stylePers.persNotsButtonText}>
-                Показать все...
-              </Text>
+                  Показать все...
+                </Text>
               </ImageBackground>
-            {/* </LinearGradient> */}
+              {/* </LinearGradient> */}
             </Pressable>
-            
+          </View>
+          <View style={[stylePers.persLevels,stylePers.wrapSearch]}>
+            <View style={stylePers.wrapSearchTeacher}>
+              <Text style={stylePers.searchTeacherText}>Поиск {'\n'}преподавателя</Text> 
+              <Image source={require('../assets/magnifier.png')} style={stylePers.searchMagnifier}/>
+            </View>
+            <View style={stylePers.wrapSearchObject}>
+              <Text style={stylePers.searchTeacherText}>Поиск {'\n'}предмета</Text> 
+              <Image source={require('../assets/magnifier.png')} style={stylePers.searchMagnifier}/>
+            </View>
+          </View>
+          <View  style={[stylePers.persLevels,stylePers.wrapCabinet]}>
+          <ImageBackground
+                source={require("../assets/persBGCabinet.png")}
+                imageStyle={{borderRadius: 20}}
+                style={[
+                  { width: "100%", height: "100%" },
+                  stylePers.persBGImage,
+                ]}
+              >
+                <View style={stylePers.wrapSearchCabinet}>
+                  <Text style={stylePers.WhereCabinet}>Где этот кабинет? :о</Text><Text style={stylePers.openMap}>Открыть карту</Text>
+                  <Image source={require('../assets/mapPin.png')} style={stylePers.mapImg}/>
+                </View>
+                </ImageBackground>
           </View>
         </View>
       </LinearGradient>
--- styles/persRoom.js
+++ styles/persRoom.js
@@ -27,12 +27,16 @@ export const stylePers = StyleSheet.create({
     paddingVertical: 40,
     justifyContent: "space-between",
   },
+  persLevels: {
+    width: "85%",
+    marginHorizontal: "auto",
+    marginBottom: 40
+  },
   persNots: {
     backgroundColor: "#91B1D7",
     borderRadius: 20,
-    width: "80%",
     height: 175,
-    marginHorizontal: "auto",
+
     shadowOpacity: 0.75,
     shadowRadius: 0,
     shadowColor: "#28466A",
@@ -44,7 +48,7 @@ export const stylePers = StyleSheet.create({
     backgroundColor: "#91B1D7",
     borderRadius: 20,
     width: "80%",
-    height: 175,
+    // height: 175,
     marginHorizontal: "auto",
     flex: 7,
   },
@@ -55,20 +59,89 @@ export const stylePers = StyleSheet.create({
     flex: 2,
     borderBottomEndRadius: 20,
     borderBottomStartRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
     zIndex: 100,
-    overflow: 'hidden'
+    overflow: "hidden",
   },
 
   persNotsButtonText: {
-    color: '#fff'
+    color: "#fff",
   },
-  persBGImage:{
-      alignItems: 'center',
-      justifyContent: "center",
+  persBGImage: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  wrapSearch: {
+    // marginTop: 40,
+    width: "85%",
+    // height: 175,
+    marginHorizontal: "auto",
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+    // width: '100%'
+  },
+  wrapSearchTeacher: {
+    backgroundColor: "#fff",
+    width: "55%",
+    height: 77,
+    borderRadius: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    textAlign: "left",
+  },
+  searchTeacherText: {
+    fontFamily: "os_reg",
+    fontSize: 17,
+  },
+  searchMagnifier: {
+    position: "absolute",
+    top: 10,
+    left: "105%",
+    resizeMode: "stretch",
+    width: 18,
+    height: 18,
+  },
+  wrapSearchObject: {
+    backgroundColor: "#fff",
+    width: "40%",
+    height: 77,
+    borderRadius: 20,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    textAlign: "left",
+  },
+  wrapCabinet: {
+backgroundColor: "#fff",
+    width: "95%",
+    height: 65,
+    borderRadius: 20,
+    // paddingVertical: 0,
+    justifyContent: 'center',
     
-  
+
+   
+  },
+  wrapSearchCabinet: {
+    paddingHorizontal: 20,
+     flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  openMap:{
+    fontFamily: 'os_reg',
+    fontSize: 13,
+    color: '#4579B5',
+  },
+  WhereCabinet:{
+    color: '#000',
+    fontFamily: 'os_reg',
+    fontSize: 17,
+    flex: 1
+  },
+  mapImg:{
+    width: 25,
+    height: 25
   }
 })

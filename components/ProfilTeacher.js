8b9f0908b6bb8a7615be6a329c79a41d9629f46f
anastas4483 <nastasya.sorokina1@gmail.com>
2022-02-14 13:16:10 +0300

    completed block photo,name,contacts in ProfilTeacher

--- components/ProfilTeacher.js
+++ components/ProfilTeacher.js
@@ -1,16 +1,18 @@
 import { Text, View, Image, Pressable } from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
 import { useState } from "react"
-
+import { stylePT } from "../styles/profilTeacher"
 import { TextInput } from "react-native-web"
 import { gStyle } from "../styles/gStyle"
+import { symbolicateLogLazy } from "react-native/Libraries/LogBox/Data/LogBoxData"
 export default function ProfilTeacher() {
   return (
     <View style={gStyle.container}>
       <LinearGradient
         colors={["#3F70A8", "#4B84C5", "#4F8BD0"]}
         style={gStyle.gradient}
-        start={{x: 0, y: 1}} end={{x: 0, y: -1}}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 0, y: -1 }}
       >
         <View style={gStyle.persHeader}>
           <Text style={gStyle.headerText}>Добрый день, Андрей!</Text>
@@ -20,6 +22,31 @@ export default function ProfilTeacher() {
               style={gStyle.persHeaderGear}
             />
           </Pressable>
+        </View>
+        <View style={stylePT.wrap}>
+          <View style={stylePT.wrapBack}>
+            <Image
+              source={require("../assets/backArrow.png")}
+              style={{ width: 27, height: 24 }}
+            />
+            <Text style={stylePT.BackText}>Назад</Text>
+          </View>
+          <View style={stylePT.wrapInfo}>
+              <View style={stylePT.wrapNamePhoto}> 
+                  <Image source={require('../assets/paretskikhEV.jpg')} style={{width: 110,height: 110}}/>
+              <View style={stylePT.wrapContacts}>
+                  <Text  style={stylePT.nameInfo}>Парецких{'\n'}Елена Викторовна</Text>
+                  <Text style={stylePT.mailInfo}>elenaparetskih@gmail.com</Text>
+                  <View style={stylePT.wrapSoc}>
+                      <Image source={require('../assets/vk.png')} style={{width: 34, height: 34}} />
+                      <Image source={require('../assets/discord.png')} style={{width: 34, height: 34}} />
+ 
+                  </View>
+              </View>
+              
+              </View>
+              
+          </View>
         </View>
       </LinearGradient>
     </View>
--- styles/profilTeacher.js
+++ styles/profilTeacher.js
@@ -1,7 +1,62 @@
 import { StyleSheet, Platform } from "react-native"
 import { G } from "react-native-svg"
 
-export const gStyle = StyleSheet.create({
+export const stylePT = StyleSheet.create({
+    wrap:{
+        paddingHorizontal: 20,
+        paddingVertical: 40
+    },
+    wrapBack:{
+        flexDirection: 'row',
+        alignItems: 'center',
+
+    },
+    BackText:{
+        fontFamily: 'os_reg',
+        fontSize: 16,
+        textDecorationLine: 'underline',
+        marginLeft: 5,
+
+    },
+    wrapInfo:{
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        width: '100%',
+        height: '85%',
+        marginTop: 30,
+        paddingVertical: 50,
+        paddingHorizontal: 8,
+
+    },
+    nameInfo:{
+        fontFamily: 'sanst_reg',
+        fontSize: 21,
+        color: '#4B4B4B',
+        lineHeight: 27,
+        letterSpacing: 0
+
+    },
+    wrapNamePhoto:{
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    wrapContacts:{
+        marginLeft: 10
+    },
+    mailInfo:{
+        color: '#969696',
+        textDecorationLine: 'underline',
+        fontSize: 13,
+        fontFamily: 'os_reg',
+        letterSpacing: 0,
+        marginTop: 10
+    },
+    wrapSoc:{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '40%',
+        marginTop: 6
+    }
 
 
 })

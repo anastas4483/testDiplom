a52035b27afa064e7452684f016a3217b5e45df8
anastas4483 <nastasya.sorokina1@gmail.com>
2022-02-15 15:07:06 +0300

    made timetable in SearchSubjects

--- components/SearchSubjects.js
+++ components/SearchSubjects.js
@@ -2,7 +2,7 @@ import { Text, View, Image, Pressable } from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
 import { useState } from "react"
 import { styleSearchS } from "../styles/searchSubjects"
-import { FlatList, TextInput } from "react-native-web"
+import { Button, FlatList, TextInput } from "react-native-web"
 import { gStyle } from "../styles/gStyle"
 import GoBackBtn from "./GoBackBtn"
 import Header from "./Header"
@@ -16,12 +16,61 @@ export default function SearchSubjects() {
         end={{ x: 0, y: -1 }}
       >
         <Header />
-          <GoBackBtn />
+        <GoBackBtn />
 
-          <View style={styleSearchS.wrap}>
-            
-            <Text style={[gStyle.H3,styleSearchS.nameSubject]}>Переферийные устройства</Text>
+        <View style={styleSearchS.wrap}>
+          <Text style={[gStyle.H3, styleSearchS.nameSubject]}>
+            Переферийные устройства
+          </Text>
+          <View style={styleSearchS.infoSubject}>
+            <View style={styleSearchS.wrapRow}>
+              <Text style={styleSearchS.character}>Преподаватель: </Text>
+              <Text style={[styleSearchS.value, styleSearchS.valueTeacher]}>
+                Парецких Е.В.
+              </Text>
+            </View>
+            <View style={styleSearchS.wrapRow}>
+              <Text style={styleSearchS.character}>Продолжительность:</Text>
+              <Text style={styleSearchS.value}>180ч</Text>
+            </View>
+            <View style={styleSearchS.wrapRow}>
+              <Text style={styleSearchS.character}>Тип сдачи: </Text>
+              <Text style={styleSearchS.value}>экзамен</Text>
+            </View>
+            <View style={styleSearchS.timetable}>
+              <Text style={styleSearchS.timetableTitle}>График занятий</Text>
+              <View style={styleSearchS.row}>
+                <Text style={styleSearchS.tableTitle}>Лекции:</Text>
+                <Text style={styleSearchS.cabinet}>401каб</Text>
+              </View>
+              <View style={styleSearchS.row}>
+                <Text style={styleSearchS.tableText}>
+                  чт - 12:00, пт - 10:15
+                </Text>
+              </View>
+              <View style={styleSearchS.row}>
+                <Text style={styleSearchS.tableTitle}>Практические:</Text>
+                <Text style={styleSearchS.cabinet}>107каб</Text>
+              </View>
+              <View style={styleSearchS.row}>
+                <Text style={styleSearchS.tableText}>
+                  1подгр: пт - 12:00 (1,3,5,7,9 нед.)
+                </Text>
+              </View>
+              <View style={styleSearchS.row}>
+                <Text style={styleSearchS.tableText}>
+                  2подгр: пт - 12:00 (2,4,6,8,10 нед.)
+                </Text>
+              </View>
+            </View>
+            <Button
+  onPress={()=>console.log('dks')}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
           </View>
+        </View>
       </LinearGradient>
     </View>
   )
--- styles/searchSubjects.js
+++ styles/searchSubjects.js
@@ -10,7 +10,69 @@ export const styleSearchS = StyleSheet.create({
     nameSubject:{
         color: '#fff',
         fontFamily: 'sansat_reg',
+
+    },
+    infoSubject:{
+        marginTop: 15,
+        borderRadius: 20,
+        backgroundColor: '#fff',
+        width: '100%',
+        height: '75%',
+        padding: 25
+    },
+    wrapRow:{
+        flexDirection: 'row',
+        marginBottom: 15,
+
+    },
+    character:{
+        color: '#6C5D5D',
+        fontFamily: 'os_reg',
+        fontSize: 17
+    },
+    value:{
+        color: '#000',
+        fontFamily: 'os_reg',
+        fontSize: 17
+    },
+    valueTeacher:{
+        textDecorationLine:'underline',
+        fontStyle: 'italic',
+    },
+    row:{
+        flexDirection: 'row',
+        justifyContent:'space-between'
+    },
+    cabinet:{
+        color: '#A7A7A7',
+        fontFamily: 'os_reg',
+        fontSize: 15,
+        marginTop: 10
+
+    },
+    tableTitle:{
+        color: '#000',
+        fontFamily: 'os_reg',
+        fontSize: 16,
+        marginTop: 10
+
+    },
+    tableText:{
+        fontFamily: 'os_reg',
+        fontSize: 14
+    },
+    timetable:{
+        borderTopColor: '#7A7A7A',
+        borderTopWidth: 1,
+    },
+    timetableTitle:{
+        fontFamily: 'os_reg',
+        fontSize: 15,
+        color: '#757575',
+        textAlign: 'center',
+        marginTop: 5
     }
+
 
 
 

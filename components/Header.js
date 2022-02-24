2ea7c4cd7bec47a66e56f682a7a70f20d483620d
anastas4483 <nastasya.sorokina1@gmail.com>
2022-02-24 18:04:16 +0300

    completed firebase auth with email/pass and logout

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,28 @@
 import { Text, View, Image, Pressable } from "react-native"
+import { auth } from "../base"
 import { gStyle } from "../styles/gStyle"
+import {useNavigation} from '@react-navigation/core'
+
 export default function Header() {
+  const navigation=useNavigation()
+
+ const exitHandler=()=>{
+  auth
+  .signOut()
+  .then(()=>{
+    navigation.replace('Login')
+  })
+  .catch(error => alert(error.message))
+ }
   return (
 <View style={gStyle.header}>
           <Text style={gStyle.headerText}>Добрый день, Андрей!</Text>
-          <Pressable>
-            <Image
+          <Pressable onPress={exitHandler}>
+            {/* <Image
               source={require("../assets/gear.png")}
               style={gStyle.headerGear}
-            />
+            /> */}
+            <Text> Выйти</Text>
           </Pressable>
         </View>
 
